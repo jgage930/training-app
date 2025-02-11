a1972fb02295344d6101ae0b8365ae4cbd5e73c3
jgage930 <jgage930@gmail.com>
2025-02-10 20:33:38 -0600

    watch

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,3 +1,3 @@
-watch --chgexit -n 1 \
+watch -n 1 \
     "ls -l --all --recursive --full-time | grep -vE node_modules | sha256sum" \
-    && echo "Detected the modification of a file or directory"
+    && ./run.sh
