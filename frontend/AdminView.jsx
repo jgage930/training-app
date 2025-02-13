25b88f7a2e20152ee4de2cd5d724d76ecb31e132
jgage930 <jgage930@gmail.com>
2025-02-12 19:24:29 -0600

    fetch api data

--- frontend/AdminView.jsx
+++ frontend/AdminView.jsx
@@ -2,7 +2,25 @@ import 'rsuite/dist/rsuite.min.css';
 import React, { useState } from 'react'
 
 function AdminView() {
-  return <div>Admin</div>
+  // Call endpoint to get workouts from db.
+  const [workouts, setWorkouts] = useState([]);
+  
+  useEffect(() => {
+    fetch('/workout', {
+      method: "GET" 
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((workouts) => {
+        const parsedObjects = workouts.map(w => ({...w, date: new Date(w.date)}));
+        setWorkouts(parsedObjects)
+    });
+  }, []);
+
+  return (
+    
+  )
 }
 
 export default AdminView;
