7387afbbf42f39112c8eb1d3da054ed6e39a2efc
jgage930 <jgage930@gmail.com>
2025-02-14 12:38:03 -0600

    read fit file

--- api/activity.go
+++ api/activity.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/jmoiron/sqlx"
 	"github.com/muktihari/fit/decoder"
+	"github.com/muktihari/fit/profile/filedef"
 )
 
 type ActivityHandler struct {
@@ -62,6 +63,30 @@ func (h *ActivityHandler) getActivity(w http.ResponseWriter, r *http.Request) {
 	readFitFile("activity_data/2024-08-26-14-45-27.fit")
 }
 
+// func readFitFile(path string) {
+// 	f, err := os.Open(path)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	defer f.Close()
+//
+// 	dec := decoder.New(f)
+//
+// 	fit, err := dec.Decode()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+//
+// 	log.Printf("FileHeader DataSize: %d\n", fit.FileHeader.DataSize)
+// 	log.Printf("Messages count: %d\n", len(fit.Messages))
+// 	// FileId is always the first message; 4 = activity
+//
+// 	// We want to track both activity and courses.
+// 	// message = 4 or 5
+// 	log.Printf("File Type: %v\n", fit.Messages[5]) //.FieldValueByNum(fieldnum.FileIdType).Any())
+//
+// }
+
 func readFitFile(path string) {
 	f, err := os.Open(path)
 	if err != nil {
@@ -69,24 +94,40 @@ func readFitFile(path string) {
 	}
 	defer f.Close()
 
-	dec := decoder.New(f)
+	// The listener will receive every message from the decoder
+	// as soon as it is decoded and transform it into an filedef.File.
+	lis := filedef.NewListener()
+	defer lis.Close() // release channel used by listener
 
-	fit, err := dec.Decode()
+	dec := decoder.New(f,
+		// Add activity listener to the decoder:
+		decoder.WithMesgListener(lis),
+		// Direct the decoder to only broadcast
+		// the messages without retaining them:
+		decoder.WithBroadcastOnly(),
+	)
+
+	_, err = dec.Decode()
 	if err != nil {
 		panic(err)
 	}
 
-	log.Printf("FileHeader DataSize: %d\n", fit.FileHeader.DataSize)
-	log.Printf("Messages count: %d\n", len(fit.Messages))
-	// FileId is always the first message; 4 = activity
-
-	// We want to track both activity and courses.
-	// message = 4 or 5
-	log.Printf("File Type: %v\n", fit.Messages[5]) //.FieldValueByNum(fieldnum.FileIdType).Any())
-
-	activites := make([]fit.Message, 0)
-	for _, message := range fit.Messages {
-
+	file, ok := lis.File().(*filedef.Activity)
+	if !ok {
+		log.Printf("%T is not an Activity File\n", lis.File())
+		return
 	}
 
+	log.Printf("File Type: %s\n", file.FileId.Type)
+	log.Printf("Sessions count: %d\n", len(file.Sessions))
+	log.Printf("Laps count: %d\n", len(file.Laps))
+	log.Printf("Records count: %d\n", len(file.Records))
+	i := 100
+	log.Printf("\nSample value of record[%d]:\n", i)
+	log.Printf("  Distance: %g m\n", file.Records[i].DistanceScaled())
+	log.Printf("  Lat: %g degrees\n", file.Records[i].PositionLatDegrees())
+	log.Printf("  Long: %g degrees\n", file.Records[i].PositionLongDegrees())
+	log.Printf("  Speed: %g m/s\n", file.Records[i].SpeedScaled())
+	log.Printf("  HeartRate: %d bpm\n", file.Records[i].HeartRate)
+	log.Printf("  Cadence: %d rpm\n", file.Records[i].Cadence)
 }
